de6f17b373ed7bfdbe26a29b4bc4d0735e9f5b16
Usman Qidwai <usmanqidwai21@gmail.com>
2023-09-27 15:39:06 -0400

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+function superbowlWin(array) {
+    
+    const winningYear = array.find(obj => obj.result === "W");
+  
+
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
